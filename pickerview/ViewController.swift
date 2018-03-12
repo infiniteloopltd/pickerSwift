c2009fae43a2da431e56c93834f5b33a82ef43f6
Fiach Reid <fiach.reid@gmail.com>
2018-03-12 15:10:51 +0000

    Initial Commit

---
+++ pickerview/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  pickerview
+//
+//  Created by user909680 on 3/12/18.
+//  Copyright © 2018 user909680. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+   
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+
+    @IBOutlet weak var picker: UIPickerView!
+    
+     var pickerData: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
